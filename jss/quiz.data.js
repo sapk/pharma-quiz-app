189a5d03efe3316aedd5437e4043e2bb0c303b81
Antoine GIRARD <sapk@sapk.fr>
2014-01-23 03:41:26 +0100

    Optimisation de l'algo de génération de mauvaise réponse

--- jss/quiz.data.js
+++ jss/quiz.data.js
@@ -5,7 +5,7 @@ Q.data = {
         {name : "Se ou Pl Créatinine - Femme", value : ["45 - 105 µmol/L","5 - 12 mg/L"]},
         {name : "Se ou Pl Urate - Homme", value : ["180 - 420 µmol/L","30 - 70 mg/L"]},
         {name : "Se ou Pl Urate - Femme", value : ["150 - 360 µmol/L","25 - 60 mg/L"]},
-        {name : "Se ou Pl Urée", value : ["2,5 - 7,5 mmol/L","0,15 - 0,45 g/L"]},
+        {name : "Se ou Pl Urée", value : ["2.5 - 7.5 mmol/L","0.15 - 0.45 g/L"]},
     ]
 };
 //http://www.cnci.univ-paris5.fr/pharmacie/Constantes_biologiques_adultes_2009.pdf
--- jss/quiz.js
+++ jss/quiz.js
@@ -9,11 +9,17 @@ var Q ={
         //ajout d'une bonne réponse au questions
         q.push([1,d.value[Math.floor((Math.random()*d.value.length))]])
         var ext = q[0][1].split(" ")[3];
-        var diff = parseFloat(q[0][1].split(" ")[2] - q[0][1].split(" ")[0]);
+        var diff = parseFloat(q[0][1].split(" ")[2]) - parseFloat(q[0][1].split(" ")[0]);
+        var pres = 0;
+        if(q[0][1].split(" ")[2].split(".").length == 2)
+            pres = q[0][1].split(" ")[2].split(".")[1].length;
+        console.log(pres);
         for (t=0;t<3;t++){
             //TODO genrate some false value
-            var min = Math.floor((Math.random()*120))
-            var text = min+ " - "+ (parseFloat(min)+parseFloat(diff)) + " " + ext;
+            var min = (Math.floor((Math.random()*parseFloat(q[0][1].split(" ")[0])*4*1000))/1000);
+            console.log(min.toFixed(pres));
+            console.log(diff);
+            var text = min.toFixed(pres)+ " - "+ (min + diff).toFixed(pres) + " " + ext;
             q.push([0,text])
         }
         
