b0869003b7fd20b9347dcc05c3181869e5116e1d
Antoine GIRARD <sapk@sapk.fr>
2014-01-27 00:02:00 +0100

    Somme corrections

--- jss/quiz.tmpl.js
+++ jss/quiz.tmpl.js
@@ -7,7 +7,7 @@ Q.tmpl = {
                 $(question.value).each(function(id, value) {
                     //ext += '<option>'+Q.tool.get_unit(value.split(" "))+'</option>';
                     list.push(Q.tool.get_unit(value.split(" ")));
-                    tmp += '<div class="valid row well answer type-' + question.type + '">' + Q.tmpl.answer.type[question.type](value.split(" ")) + '</div>'
+                    tmp += '<div class="valid row well well-sm answer type-' + question.type + '">' + Q.tmpl.answer.type[question.type](value.split(" ")) + '</div>'
                 });
                 for (t = list.length; t < 5; t++) {
                    // console.log(t);
@@ -21,7 +21,7 @@ Q.tmpl = {
                     ext += '<option>' + value + '</option>';
                 });
 
-                html += '<div class="row well answer type-' + question.type + '">' + Q.tmpl.answer.type[question.type]([], ext) + '</div>'
+                html += '<div class="row well well-sm answer type-' + question.type + '">' + Q.tmpl.answer.type[question.type]([], ext) + '</div>'
                 html += tmp;
                 html += '<br><br><br>';
                 html += '<button id="valider" onclick="$(\'.answer\').toggleClass(\'results\');$(\'#reload\').show();$(this).remove();" class="btn btn-lg btn-block btn-primary pull-right"> Valider > </button>\
@@ -33,15 +33,15 @@ Q.tmpl = {
         type: {
             1: function(value, ext) {
                 if (value.length === 0) {
-                    return '<div class="col-xs-3" style="margin-left: 4.16%;"><input class="form-control input-lg" id="0"/></div>\n\
+                    return '<div class="col-xs-4"><input class="form-control input-lg" id="0"/></div>\n\
                         <div class="col-xs-1" > - </div>\n\
-                        <div class="col-xs-3"><input id="1" class="form-control input-lg"/></div>\n\
-                        <div class="col-xs-3 col-xs-offset-1"><select class="form-control input-lg" >' + ext + '</select></div>';
+                        <div class="col-xs-4"><input id="1" class="form-control input-lg"/></div>\n\
+                        <div class="col-xs-3"><select class="form-control input-lg" >' + ext + '</select></div>';
                 } else {
-                    return '<div class="col-xs-3" style="margin-left: 4.16%;"><input class="form-control input-lg" id="0" value="' + value[0] + '"/></div>\n\
+                    return '<div class="col-xs-4"><input class="form-control input-lg" id="0" value="' + value[0] + '"/></div>\n\
                         <div class="col-xs-1" > - </div>\n\
-                        <div class="col-xs-3"><input id="1" class="form-control input-lg"  value="' + value[2] + '"/></div>\n\
-                        <div class="col-xs-3 col-xs-offset-1"><select class="form-control input-lg" ><option disabled selected style="display:none;" >' + Q.tool.get_unit(value) + '</option><option>L</option></select></div>';
+                        <div class="col-xs-4"><input id="1" class="form-control input-lg"  value="' + value[2] + '"/></div>\n\
+                        <div class="col-xs-3"><select class="form-control input-lg" ><option disabled selected style="display:none;" >' + Q.tool.get_unit(value) + '</option></select></div>';
                 }
             }
         }
