26f7233e72e37dd7c0b3183a81be4c8c9547e054
Antoine GIRARD <sapk@sapk.fr>
2014-12-11 02:18:39 +0100

    ...

--- js/track.js
+++ js/track.js
@@ -1,4 +1,4 @@
-$(document).ready(window.setTimeout(track,500));    
+$(document).ready(window.setTimeout(track,1000));    
 
 function track () {
     analytics.startTrackerWithId('UA-57562500-2');
@@ -6,4 +6,6 @@ function track () {
     if($("title").text()==="Quiz Pharma"){
         analytics.trackEvent('Question', (($(".choices").length===1)?'Choice':'Answer'), $("h2.title").text());
     }
+    admobAd.initBanner("ca-app-pub-4837912223938353/9946559021", admobAd.AD_SIZE.BANNER.width, admobAd.AD_SIZE.BANNER.height);//create admob banner
+    admobAd.showBanner(admobAd.AD_POSITION.BOTTOM_CENTER);
 }
